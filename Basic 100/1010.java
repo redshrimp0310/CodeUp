98b509805cb839543dbcc5fe8f7e37d00545a13d
redshrimp0310 <74122295+redshrimp0310@users.noreply.github.com>
2020-12-16 21:18:29 +1300

    Create 1010.java

---
+++ Basic 100/1010.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // int num;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num);
+        
+    }
+}
