60965b4191357e8347ddadd65c2c7d63bd25ea34
redshrimp0310 <74122295+redshrimp0310@users.noreply.github.com>
2020-12-16 21:21:27 +1300

    Create 1017.java

---
+++ Basic 100/1017.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        System.out.printf("%d %d %d",num,num,num);
+    }
+}
