74ec0fcef0cf6a5b5f11a4775b0798c8644538c2
redshrimp0310 <74122295+redshrimp0310@users.noreply.github.com>
2020-12-16 21:22:50 +1300

    Create 1021.java

---
+++ Basic 100/1021.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	String s = sc.nextLine();
+//    	System.out.printf("%s",s);
+
+    	
+    	if(s.length() < 51) {
+    		System.out.printf("%s",s);
+    		
+    	} else {}
+    	
+    }
+}
+
+
+
