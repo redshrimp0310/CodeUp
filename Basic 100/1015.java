db80368297953d3ad8b57fa389e1b9a502d293f1
redshrimp0310 <74122295+redshrimp0310@users.noreply.github.com>
2020-12-16 21:20:59 +1300

    Create 1015.java

---
+++ Basic 100/1015.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float f;
+        Scanner sc = new Scanner(System.in);
+        f = sc.nextFloat();
+        System.out.printf("%.2f",f);
+    }
+}
