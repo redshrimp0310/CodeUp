e725a5d43604ae737f57a35785e4bc076a5b0384
redshrimp0310 <74122295+redshrimp0310@users.noreply.github.com>
2020-12-16 21:21:45 +1300

    Create 1018.java

---
+++ Basic 100/1018.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String s; 
+        Scanner sc = new Scanner(System.in);
+        s = sc.nextLine();
+        System.out.printf("%s",s);
+    }
+}
