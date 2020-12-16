9ae3a4513e1c8532a9bd37444bb181da10c79707
redshrimp0310 <74122295+redshrimp0310@users.noreply.github.com>
2020-12-16 21:20:12 +1300

    Create 1013.java

---
+++ Basic 100/1013.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        // Scanner sc2 = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.printf("%d %d", a,b);
+    }
+}
