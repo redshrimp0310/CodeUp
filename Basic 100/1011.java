f3b64e599e53ab13172c915d91a3d4d97d105c7e
redshrimp0310 <74122295+redshrimp0310@users.noreply.github.com>
2020-12-16 21:19:15 +1300

    Create 1011.java

---
+++ Basic 100/1011.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        char c;
+        Scanner sc = new Scanner(System.in);
+        c = sc.next().charAt(0);
+        System.out.printf("%c",c);
+    }
+}
